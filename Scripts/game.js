eeb1aab1628d24bc50b4850e61db3b1a377bc9ec
garv420 <garvit.23bcs10091@ms.sst.scaler.com>
2024-05-21 23:38:02 +0530

    initial commit

---
+++ Scripts/game.js
@@ -0,0 +1,101 @@
+let questions = [
+    {
+        question: 'Which HTML tag is used to define an inline style?',
+        choice1: '<script>',
+        choice2: '<css>',
+        choice3: '<style>',
+        choice4: '<span>',
+        answer: 3,
+    },
+    {
+        question: 'Which property is used to change the text color in CSS?',
+        choice1: 'text-color',
+        choice2: 'font-color',
+        choice3: 'text-style',
+        choice4: 'color',
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is the correct way to comment in HTML?',
+        choice1: '// Comment',
+        choice2: '<!-- Comment -->',
+        choice3: '/* Comment */',
+        choice4: '<! Comment>',
+        answer: 2,
+    },
+];
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let questionCounter = 0;
+let score = 0;
+const SCORE_POINTS = 10;
+const MAX_QUESTIONS = questions.length;
+let availableQuestions = [...questions];
+
+const questionElement = document.getElementById('question');
+const choiceElements = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+
+const startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+const getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('end.html');
+    }
+
+    questionCounter++;
+    progressText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    questionElement.innerText = currentQuestion.question;
+
+    choiceElements.forEach(choice => {
+        const number = choice.parentElement.getAttribute('data-number');
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+const incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+};
+
+choiceElements.forEach(choice => {
+    choice.parentElement.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.parentElement.getAttribute('data-number');
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS);
+        }
+
+        localStorage.setItem('mostRecentScore', score);
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000); 
+    });
+});
+
+startGame();
